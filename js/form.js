746a719a70b6b69b5a72a5f3e094273cab08646f
chofapkdmnt <62484878+chofapkdmnt@users.noreply.github.com>
2020-09-08 11:31:44 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@ document.querySelector('#submit').addEventListener('click',function(){
     let barbero = document.querySelector('#barbero').value;
     let servicio = document.querySelector('#servicio').value;
 
-    let url = "https://api.whatsapp.com/send?phone=573105010573&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
+    let url = "https://api.whatsapp.com/send?phone=523411061011&text=*_Barberia Lider_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
     window.open(url);
 
 });
